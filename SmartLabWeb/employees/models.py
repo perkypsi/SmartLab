4debdb2c2a8642bd25bf03ed7e1d14ce55f434db
Etsushiki <disakovforrhtu@gmail.com>
2024-03-29 19:48:07 +0300

    add models of projects and empoyees

--- SmartLabWeb/employees/models.py
+++ SmartLabWeb/employees/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class employee(models.Model):
+    fullname = models.CharField("Фамилия")
--- SmartLabWeb/projects/models.py
+++ SmartLabWeb/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from employees.models import employee
 
-# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField('Название проекта', max_length = 255)
+    description_template = models.TextField("Описание проекта")
+    member = models.ManyToManyField(employee)
+    leader = models.ManyToManyField(employee)
+    create_date = models.DateTimeField('Дата создания')
