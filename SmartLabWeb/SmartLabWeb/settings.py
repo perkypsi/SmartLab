50a398bd06ea532081a9f867f57ce9c4faa10d13
Etsushiki <disakovforrhtu@gmail.com>
2024-03-29 18:09:39 +0300

    employees

--- SmartLabWeb/SmartLabWeb/settings.py
+++ SmartLabWeb/SmartLabWeb/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employess.apps.EmployeesConfig',
+    'employees.apps.EmployeesConfig',
     'protocols.apps.ProtocolsConfig',
     'projects.apps.ProjectsConfig',
     'laboratories.apps.LaboratoriesConfig'
