f0ebd88b6b889f4c4ca9df1b6ba2cbfd010db0c6
Бойко Константин Витальевич <boiko.k.v@muctr.ru>
2024-04-02 19:31:48 +0300

    add models in admin

--- SmartLabWeb/AccessManagementSystem/admin.py
+++ SmartLabWeb/AccessManagementSystem/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Enviroment, AccessLevel, Role, Profile, Project, Notification
+
+class ProfileAdmin(UserAdmin):
+    model = Profile
+    list_display = ['patronymic', 'username']
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Enviroment)
+admin.site.register(AccessLevel)
+admin.site.register(Role)
+admin.site.register(Project)
+admin.site.register(Notification)
--- SmartLabWeb/ResourceInventory/admin.py
+++ SmartLabWeb/ResourceInventory/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, TypeConsumables, TypeEquipment, Consumables, Equipment, ActionField, ProcedureTemplate, Procedure, Sample, Analysis
+
+admin.site.register(Document)
+admin.site.register(TypeConsumables)
+admin.site.register(TypeEquipment)
+admin.site.register(Consumables)
+admin.site.register(Equipment)
+admin.site.register(ActionField)
+admin.site.register(ProcedureTemplate)
+admin.site.register(Procedure)
+admin.site.register(Sample)
+admin.site.register(Analysis)
+
